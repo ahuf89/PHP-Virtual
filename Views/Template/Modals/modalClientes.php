390160614b78005d77f14bb90b15569c45fba634
alvarouseche <jboalvarouseche@gmail.com>
2022-06-28 16:58:05 -0500

    update graphics

--- Views/Template/Modals/modalClientes.php
+++ Views/Template/Modals/modalClientes.php
@@ -42,19 +42,19 @@
                 </div>
               </div>
               <hr>
-              <p class="text-primary">Datos Fiscales.</p>
+              <p class="text-primary">Datos.</p>
               <div class="form-row">
                 <div class="form-group col-md-6">
-                  <label>Identificación Tributaria <span class="required">*</span></label>
+                  <label>NIT<span class="required">*</span></label>
                   <input class="form-control" type="text" id="txtNit" name="txtNit" required="">
                 </div>
                 <div class="form-group col-md-6">
-                  <label>Nombre fiscal <span class="required">*</span></label>
+                  <label>Nombre <span class="required">*</span></label>
                   <input class="form-control" type="text" id="txtNombreFiscal" name="txtNombreFiscal" required="">
                 </div>
 
                 <div class="form-group col-md-12">
-                  <label>Dirección fiscal <span class="required">*</span></label>
+                  <label>Dirección<span class="required">*</span></label>
                   <input class="form-control" type="text" id="txtDirFiscal" name="txtDirFiscal" required="">
                 </div>
                 
